3b43fb4dd7e3e17233e7d51067d0a9136b39f63d
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2022-02-19 21:11:11 +0300

    feat: add sprint 2 draft, add block and eventBus drafts

---
+++ src/components/Block/Block.ts
@@ -0,0 +1,121 @@
+import { EventBus } from '../EventBus';
+
+export abstract class Block {
+  private static EVENTS = {
+    INIT: "init",
+    FLOW_CDM: "flow:component-did-mount",
+    FLOW_CDU: "flow:component-did-update",
+    FLOW_RENDER: "flow:render"
+  };
+  private _element: HTMLElement;
+  private readonly _meta: {
+    tagName: string,
+    props: Record<string, unknown>
+  };
+  private eventBus: () => EventBus;
+  public props: Record<string, unknown>;
+
+  constructor(tagName = "div", props = {}) {
+    const eventBus = new EventBus();
+    this._meta = {
+      tagName,
+      props
+    };
+
+    this.props = this._makePropsProxy(props);
+    this.eventBus = () => eventBus;
+    this._registerEvents(eventBus);
+    eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  private _registerEvents(eventBus: EventBus) {
+    eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+  }
+
+  private _createResources() {
+    const { tagName } = this._meta;
+    this._element = this._createDocumentElement(tagName);
+  }
+
+  init() {
+    this._createResources();
+    this.eventBus().emit(Block.EVENTS.FLOW_CDM);
+  }
+
+  private _componentDidMount() {
+    this.componentDidMount();
+    this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+  }
+
+  componentDidMount() {
+  }
+
+  private _componentDidUpdate(oldProps: Record<string, unknown>, newProps: Record<string, unknown>) {
+    const response = this.componentDidUpdate(oldProps, newProps);
+    if (!response) {
+      return;
+    }
+    this._render();
+  }
+
+  public componentDidUpdate(oldProps: Record<string, unknown>, newProps: Record<string, unknown>) {
+    return oldProps !== newProps;
+  }
+
+  public setProps = (nextProps: Record<string, unknown>) => {
+    if (!nextProps) {
+      return;
+    }
+
+    Object.assign(this.props, nextProps);
+  };
+
+  get element() {
+    return this._element;
+  }
+
+  private _render() {
+    const block = this.render();
+    this._element.innerHTML = block;
+  }
+
+  abstract render(): string;
+
+  getContent() {
+    return this.element;
+  }
+
+  private _makePropsProxy(props: Record<string, unknown>) {
+    const self = this;
+
+    return new Proxy(props, {
+      get(target: Record<string, unknown>, prop: string) {
+        const value = target[prop];
+        return typeof value === "function" ? value.bind(target) : value;
+      },
+      set(target: Record<string, unknown>, prop: string, value: unknown) {
+        target[prop] = value;
+        self.eventBus().emit(Block.EVENTS.FLOW_CDU, { ...target }, target);
+        return true;
+      },
+      deleteProperty() {
+        throw new Error("Нет доступа");
+      }
+    });
+  }
+
+  private _createDocumentElement(tagName: string) {
+    return document.createElement(tagName);
+  }
+
+  show() {
+    this.getContent().style.display = "block";
+  }
+
+  hide() {
+    this.getContent().style.display = "none";
+  }
+}
