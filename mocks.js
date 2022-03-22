a20ce578c485d0acdabc4c81a64424ef64b279d0
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2022-03-22 13:19:28 +0300

    feat: add router test

--- mocks.js
+++ mocks.js
@@ -1,6 +1,6 @@
 require.extensions['.scss'] = function () { return null; };
 const { JSDOM } = require('jsdom');
-const { window } = new JSDOM('<div id="page"></div>', { url: 'http://localhost:3000' });
+const { window } = new JSDOM('<div class="page"></div>', { url: 'http://localhost:3000' });
 
 global.window = window;
 global.document = window.document;
